c5ce5e42f3ee9c44dabc1f831667331897d57064
TUT3Software4Project <tut.umemura.lab@gmail.com>
2020-11-30 07:54:24 +0900

    User Created

---
+++ s4/B181302/Hello.java
@@ -0,0 +1,9 @@
+package s4.B181302;
+import java.lang.*;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello..(B181302)");
+    }
+}
+
---
+++ s4/B203356/Hello.java
@@ -0,0 +1,9 @@
+package s4.B203356;
+import java.lang.*;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello..(B203356)");
+    }
+}
+
