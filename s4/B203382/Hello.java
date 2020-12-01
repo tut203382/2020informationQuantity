fc0c133cceb66b790f95c56485da7196fbe8d55a
tut203382 <herrera.prieto.omar.nelson.ii@tut.jp>
2020-12-01 16:00:15 +0900

    Name changed

--- s4/B203382/Hello.java
+++ s4/B203382/Hello.java
@@ -3,7 +3,7 @@ import java.lang.*;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello...Herrera Prieto Omar Nelson");
+        System.out.println("Hello...Herrera Prieto Omar Nelson");
     }
 }
 
